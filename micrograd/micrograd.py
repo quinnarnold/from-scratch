8073e50e872d9a9bdcf75375aaec083ec85012e6
quinnarnold <qarnold005@gmail.com>
2026-02-09 21:49:06 -0500

    Day #2 done. Should finish by tomorrow. All of micrograd is done, just need to finish parts of the video where we build out a multi layer neuron, aka just multiple neurons, as this has all been for one neuron.
    I was able to build micrograd from memory today, after finishing its
    tutorial in the video. I was occosionally confused by the backward
    passes within functions, since there were hints of recursion which I
    sometimes struggle with, but ended up understanding.
    
    The hardest part of today was understanding a topological graph / graph
    into a list of topology order to then be reversed. Conceptually speaking
    it makes sense, just the self-reference within the definition and
    recursion that was a little difficult.
    
    Also, by chance, much of my day in classes was spent discussing topics
    which micrograd uses and references, such as these beautiful partial
    derivatives following down using recursion and the Calculus chain rule,
    as well as directional-derivatives, using gradient vectors, a point, and
    a direction vector or another point to determine the rate of change of
    that point in that direction via the gradient vector.
    This was interesting because the gradient vector is maximizing or
    minimizing, just as Machine Learning optimization functions do (which is
    structurally sound as similar methods to gradient vectors are used, or
    maybe in some frameworks they are even used themselves — will have to
    look into).
    
    Overall, very pleased with the progress of micrograd and now have a full
    conceptual understanding of back propagation within neural networks and
    other places & industries.
    
    Video End: 1:44 hours:mins in

--- micrograd/micrograd.py
+++ micrograd/micrograd.py
@@ -1,3 +1,6 @@
+##Implementing micrograd from scratch using memory and my own intuition. Code likely will not be exact. Hope to further conceptualize
+# Currently understand all steps, but gradually confusing myself the more I think about the backward pass gradient applications.
+# Implementing with no video will likely help.
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,74 +10,122 @@ class Value():
 
     def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
-        self.grad = 0.0
-        self._prev = set(_children)
+        self._prev = _children
         self._op = _op
+        self.grad = 0.0
+        self._backward = lambda: None
         self.label = label
 
     def __repr__(self):
-        return f'Value={self.data}'
+        return f"Value(data={self.data})"
 
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
+
+        ##a+b = out // dout/da = 1.0, dout/db = 1.0 // multiply by out.grad for chain rule to get proper grad
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
+
         return out
 
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
+
+        ##a*b = out // dout/da = b, dout/db = a // opposite variable of the one derivation is taking respect to // multiply by out.grad
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+
         return out
 
+    def __rmul__(self, other):
+        return -self * other
+
+    def __neg__(self):
+        return self * -1
+
     def __sub__(self, other):
-        out = Value(self.data + other.data, (self, other), '-')
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data - other.data, (self, other), '-')
+
+        ##a - b = out // dout/da = 1, dout/db = -1 // must be careful with signs // multiply by out.grad
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += -1.0 * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __pow__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data ** other.data, (self, ), f'**{other.data}')
+
+        ##Everybody knows the power rule  :)
+        def _backward():
+            self.grad += other.data * (self.data**(other.data-1)) * out.grad
+        out._backward = _backward
+        return out
+
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self,), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward
         return out
 
     def tanh(self):
         x = self.data
         p = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
         out = Value(p, (self,), 'tanh')
+        def _backward():
+            self.grad += (1 - p**2) * out.grad
+        out._backward = _backward
         return out
 
-a = Value(6.0, label='a')
-b = Value(-4.0, label='b')
-c = a-b ; c.label='c'
-d = ((c+a) - b) + a*a ; d.label='d'
-L = d * c ; L.label='L'
+    def backward(self):
+        topo = []
+        visit = set()
+        def build_topo(v):
+            if v not in visit:
+                visit.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                    visit.add(child)
+                topo.append(v)
+        build_topo(self)
 
-L.grad = 1.0
-
-def standbyderiv():
-    h = 0.0001
-    a = Value(6.0, label='a')
-    b = Value(-4.0, label='b')
-    c = a-b ; c.label='c'
-    d = ((c+a) - b) + a*a ; d.label='d'
-    L = d * c ; L.label='L'
-    L1 = L.data
-
-    a = Value(6.0+h, label='a')
-    b = Value(-4.0, label='b')
-    c = a-b ; c.label='c'
-    d = ((c+a) - b) + a*a ; d.label='d'
-    L = d * c ; L.label='L'
-    L2 = L.data
-
-    print((L2-L1) / h)
-
-standbyderiv()
+        self.grad = 1.0
+        for node in reversed(self):
+            node._backward()
 
 
-##Simple Neuron
-#inputs
-x1 = Value(5.0, label='x1')
-x2 = Value(1.0, label='x2')
-#weights
-w1 = Value(-1.0, label='w1')
-w2 = Value(4.0, label='w2')
-#bias
-b = Value(2.0, label='b')
-#Multiplication (simple matrix mult / linear equation)
-x1w1 = x1*w1; x1w1.label='x1w1'
-x2w2 = x2*w2; x2w2.label='x2w2'
-x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label='x1w1+x2w2'
-n = x1w1x2w2 + b; n.label='n'
-o = n.tanh(); o.label='o'
-print(n, o)
+        ##start with parent node, add it to set
+        #for each child in parent node, go into that child, if that child has no more children, add itself to the visit, if it has more children, go into
+        # those children, etc etc, until no further children, where then the final child adds itself to the set, then recurse backwards, taking each node out of the loop and adding
+        # itself into the visit set. Once all nodes in visit set, we append v, aka self. But within each child loop, they also not only add themself to the set, but if they dont
+        # have more children, they also add themselves to the topo list. Thus, we have a set of visits and a topology list where deepest nodes with no children
+        # occur first, and our parent occurs last. By reversing, we back propogate without errors.
+        # if child has nodes:
+            # child -> childs -> add childs to visit -> add childs to topo <- back out <- add child to visit <- add child to topo
+        # if child has no nodes:
+            # parent -> child -> add child to visit -> add child to topo <- back out <- add parent to visit <- add parent to topo
+        #even more verbose explanation:
+            # start from parent. go into child node, child node runs build topo, adds itself to visit, checks for any children
+            # if no children, adds itself to visit again, then appends itself to topo
+            # if children, go into children, etc etc, children append themselves, child appends itself to visit, to topo,
+            # parent appends self to topo
+        #beautiful representation of recursion and loops. very difficult to wrap the mind around though, since calling itself within the loop.
+
+a = Value(2.0)
+b = Value(6.0)
+c = Value(-1.0)
+
+print(a+b, a*b, b-a, b**a, b**c, b.exp(), b.tanh())
